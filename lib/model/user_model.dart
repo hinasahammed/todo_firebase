4db8a11e037b4ed43e9f066518c5bc4fd71194ea
Ahammed Hinas <82311707+hinasahammed@users.noreply.github.com>
2024-10-03 23:12:48 +0530

    Create user_model.dart

---
+++ lib/model/user_model.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class UserModel {
+  final String userName;
+  final String email;
+  final String imageUrl;
+  UserModel({required this.userName, required this.email, required this.imageUrl});
+
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'userName': userName,
+      'email': email,
+      'imageUrl': imageUrl,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      userName: map['userName'] as String,
+      email: map['email'] as String,
+      imageUrl: map['imageUrl'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserModel.fromJson(String source) => UserModel.fromMap(json.decode(source) as Map<String, dynamic>);
+}
